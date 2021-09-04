80525ae5850dda0a6db1ec0010c4a126d4f2ac60
lakis <zigras00@gmail.com>
2021-09-04 10:26:14 +0200

    define initial settings-add debug toolbar

---
+++ hotels/urls.py
@@ -0,0 +1,8 @@
+import debug_toolbar
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('admin/', admin.site.urls),
+]
