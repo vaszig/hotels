9965ccabacb5936743057e20463025d6021346af
lakis <zigras00@gmail.com>
2021-09-11 11:56:25 +0200

    refactor search_view-reduce db queries

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ def search_hotels(request):
     """Lists all hotels according to the city the user searches for."""
     results = []
     city = request.GET.get('city')
-    cities_qs = City.objects.prefetch_related('hotel_set').filter(name__icontains=city)
+    cities_qs = City.objects.filter(name__icontains=city).prefetch_related('hotel_set')
     if cities_qs.count() > 0 and len(city) > 0:
         for city in cities_qs:
             item = {'city':city.name, 'hotels':[]}
